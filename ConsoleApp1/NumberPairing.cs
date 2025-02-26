1488592695c91e254fd62c28ff7714218b058124
sonia <obedsonia8@gmail.com>
2025-02-26 09:50:27 +0700

    made NumberPairing more general so it can takes parameters

--- ConsoleApp1/NumberPairing.cs
+++ ConsoleApp1/NumberPairing.cs
@@ -1,7 +1,6 @@
-public class NumberPairing { //pairs numbers that sum to 10
-    public static void Run() {
-        List<int> numbers = [1,2,3,4,5,6,7,8,9];
-        int numbersSum = 10;
+public class NumberPairing {
+    public static void Run(List<int> numbers, int numbersSum) {
+        Console.WriteLine($"Pairs of numbers that sum to {numbersSum}");
         for (int i = 0; i < numbers.Count; i++) {
             for (int j = i + 1; j < numbers.Count; j++) {
                 if (numbers[i] + numbers[j] == numbersSum) {
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,3 @@
-﻿NumberPairing.Run();
+﻿List<int> numbers = [1,2,3,4,5,6,7,8,9];
+int numbersSum = 10;
+NumberPairing.Run(numbers, numbersSum);
