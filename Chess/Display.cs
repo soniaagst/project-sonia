7bfea0b349e4f1794c8d6f92379155e008dad18f
sonia <obedsonia8@gmail.com>
2025-03-11 12:58:32 +0700

    created Display class for Chess

---
+++ Chess/Display.cs
@@ -0,0 +1,50 @@
+public class Display {
+    public void DisplayBoard(Board board, Position? lastMovementOrigin = null) {
+        Console.WriteLine("  A  B  C  D  E  F  G  H  ");
+        for (int row = 0; row < 8; row++) {
+            Console.Write(8-row);
+            for (int col = 0; col < 8; col++) {
+                if (lastMovementOrigin?.Row == row && lastMovementOrigin?.Col == col) {
+                    Console.Write(" ＊");
+                }
+                else if (board.GetBoard()[row, col] == null) {
+                    Console.Write(" ・");
+                }
+                else {
+                    Piece piece = board.GetBoard()[row, col];
+                    if (piece!.Color is PieceColor.White) {
+                        if (piece is Pawn) Console.Write(" ♙ ");
+                        if (piece is Knight) Console.Write(" ♘ ");
+                        if (piece is Bishop) Console.Write(" ♗ ");
+                        if (piece is Rook) Console.Write(" ♖ ");
+                        if (piece is Queen) Console.Write(" ♕ ");
+                        if (piece is King) Console.Write(" ♔ ");
+                    }
+                    else {
+                        if (piece is Pawn) Console.Write(" ♟ ");
+                        if (piece is Knight) Console.Write(" ♞ ");
+                        if (piece is Bishop) Console.Write(" ♝ ");
+                        if (piece is Rook) Console.Write(" ♜ ");
+                        if (piece is Queen) Console.Write(" ♛ ");
+                        if (piece is King) Console.Write(" ♚ ");
+                    }
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public void DisplayMessage(string message) {
+        Console.WriteLine(message);
+    }
+
+    public void AskNonNullInput(string? message = null) {
+        Console.Write(message);
+        string input = Console.ReadLine();
+        while (string.IsNullOrEmpty(input)) {
+            Console.WriteLine("Input cannot empty. Try again.");
+            Console.Write(message);
+            input = Console.ReadLine();
+        }
+    }
+}
