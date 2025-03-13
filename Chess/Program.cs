6736724ba7281d7046d8c7ee264925d8419221e9
Sonia Augustin <obedsonia@outlook.com>
2025-03-14 06:28:04 +0700

    test

--- Chess/Program.cs
+++ Chess/Program.cs
@@ -1,36 +1,36 @@
-﻿// Board board = new Board();
-// Display display = new Display();
+﻿Board board = new Board();
+Display display = new Display();
 
-// //white
-// Position pawnOldpos = new Position(6,1);
-// Position pawnNewpos = new Position(4,1);
+//white
+Position pawnOldpos = new Position(6,1);
+Position pawnNewpos = new Position(4,1);
 
-// Piece? pawnPiece = board.GetPieceAt(pawnOldpos);
+Piece? pawnPiece = board.GetPieceAt(pawnOldpos);
 
 // Position? whiteOrigin = new();
-// board.MovePiece(pawnOldpos, pawnNewpos, out whiteOrigin);
+board.MovePiece(pawnOldpos, pawnNewpos, out _, out _);
 
-// display.DisplayBoard(board, whiteOrigin);
+display.DisplayBoard(board, pawnOldpos);
 
-// //black
-// Position blackPawnOldpos = new Position(1,2);
-// Position blackPawnNewpos = new Position(3,2);
+//black
+Position blackPawnOldpos = new Position(1,2);
+Position blackPawnNewpos = new Position(3,2);
 
-// Piece? blackPawnPiece = board.GetPieceAt(blackPawnOldpos);
+Piece? blackPawnPiece = board.GetPieceAt(blackPawnOldpos);
 
 // Position? blackOrigin = new();
-// board.MovePiece(blackPawnOldpos, blackPawnNewpos, out blackOrigin);
+board.MovePiece(blackPawnOldpos, blackPawnNewpos, out _, out _);
 
-// display.DisplayBoard(board, blackOrigin);
+display.DisplayBoard(board, blackPawnOldpos);
 
-// // white
-// pawnOldpos = pawnPiece!.CurrentPosition;
-// pawnNewpos = new Position(3,2);
+// white
+pawnOldpos = pawnPiece!.CurrentPosition;
+pawnNewpos = new Position(3,2);
 
-// board.MovePiece(pawnOldpos, pawnNewpos, out whiteOrigin);
+board.MovePiece(pawnOldpos, pawnNewpos, out _, out _);
 
-// display.DisplayBoard(board, whiteOrigin);
+display.DisplayBoard(board, pawnOldpos);
 
 // test ParseInput
-(Position currentpos, Position newpos) = Utility.ParseInput("enter move ");
+(Position currentpos, Position newpos) = display.ParseInputToMove("enter move ");
 Console.WriteLine($"{currentpos.Row}, {currentpos.Col} -> {newpos.Row}, {newpos.Col}");
