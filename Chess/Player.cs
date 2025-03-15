25b87950ece8efc73a4d86544379b4ae43b92d84
Sonia Augustin <obedsonia@outlook.com>
2025-03-15 16:34:56 +0700

    turned HasValidMove to be method

--- Chess/Player.cs
+++ Chess/Player.cs
@@ -2,12 +2,23 @@ public class Player {
     public string Name {get;}
     public PieceColor Color {get;}
     public PlayerStatus Status {get; set;}
-    public bool HasValidMoves {get; set;}
 
     public Player(string name, PieceColor color) {
         Name = name;
         Color = color;
         Status = PlayerStatus.Normal;
-        HasValidMoves = true;
+    }
+
+    public bool HasValidMove(Board board) {
+        List<Movement> validMoves = [];
+        foreach (var piece in board.GetBoard()) {
+            if (piece?.Color == Color) {
+                foreach (var destination in piece.GetValidMoves(board)) {
+                    validMoves.Add(new Movement(piece.CurrentPosition, destination));
+                }
+            }
+        }
+        if (validMoves != null) return true;
+        else return false;
     }
 }
