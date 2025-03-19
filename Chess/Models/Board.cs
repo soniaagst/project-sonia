cfb4ed22c306ed896bceec9805302ef8fba605b0
Sonia Augustin <obedsonia@outlook.com>
2025-03-20 05:38:44 +0700

    added SetPiece for easier board custom

--- Chess/Models/Board.cs
+++ Chess/Models/Board.cs
@@ -13,27 +13,32 @@ public class Board
 
         for (int i = 0; i < 8; i++)
         {
-            _grid[6, i] = new Pawn(PieceColor.White, new Position(6, i));
-            _grid[1, i] = new Pawn(PieceColor.Black, new Position(1, i));
+            SetPiece(new Pawn(PieceColor.White, new Position(6,i)));
+            SetPiece(new Pawn(PieceColor.Black, new Position(1,i)));
         }
 
-        _grid[7, 0] = new Rook(PieceColor.White, new Position(7, 0));
-        _grid[7, 7] = new Rook(PieceColor.White, new Position(7, 7));
-        _grid[7, 1] = new Knight(PieceColor.White, new Position(7, 1));
-        _grid[7, 6] = new Knight(PieceColor.White, new Position(7, 6));
-        _grid[7, 2] = new Bishop(PieceColor.White, new Position(7, 2));
-        _grid[7, 5] = new Bishop(PieceColor.White, new Position(7, 5));
-        _grid[7, 3] = new Queen(PieceColor.White, new Position(7, 3));
-        _grid[7, 4] = new King(PieceColor.White, new Position(7, 4));
+        SetPiece(new Rook(PieceColor.White, new Position(7, 0)));
+        SetPiece(new Rook(PieceColor.White, new Position(7, 7)));
+        SetPiece(new Knight(PieceColor.White, new Position(7, 1)));
+        SetPiece(new Knight(PieceColor.White, new Position(7, 6)));
+        SetPiece(new Bishop(PieceColor.White, new Position(7, 2)));
+        SetPiece(new Bishop(PieceColor.White, new Position(7, 5)));
+        SetPiece(new Queen(PieceColor.White, new Position(7, 3)));
+        SetPiece(new King(PieceColor.White, new Position(7, 4)));
 
-        _grid[0, 0] = new Rook(PieceColor.Black, new Position(0, 0));
-        _grid[0, 7] = new Rook(PieceColor.Black, new Position(0, 7));
-        _grid[0, 1] = new Knight(PieceColor.Black, new Position(0, 1));
-        _grid[0, 6] = new Knight(PieceColor.Black, new Position(0, 6));
-        _grid[0, 2] = new Bishop(PieceColor.Black, new Position(0, 2));
-        _grid[0, 5] = new Bishop(PieceColor.Black, new Position(0, 5));
-        _grid[0, 3] = new Queen(PieceColor.Black, new Position(0, 3));
-        _grid[0, 4] = new King(PieceColor.Black, new Position(0, 4));
+        SetPiece(new Rook(PieceColor.Black, new Position(0, 0)));
+        SetPiece(new Rook(PieceColor.Black, new Position(0, 7)));
+        SetPiece(new Knight(PieceColor.Black, new Position(0, 1)));
+        SetPiece(new Knight(PieceColor.Black, new Position(0, 6)));
+        SetPiece(new Bishop(PieceColor.Black, new Position(0, 2)));
+        SetPiece(new Bishop(PieceColor.Black, new Position(0, 5)));
+        SetPiece(new Queen(PieceColor.Black, new Position(0, 3)));
+        SetPiece(new King(PieceColor.Black, new Position(0, 4)));
+    }
+
+    public void SetPiece(Piece piece)
+    {
+        _grid[piece.CurrentPosition.Row, piece.CurrentPosition.Col] = piece;
     }
 
     public Piece?[,] GetBoard()
